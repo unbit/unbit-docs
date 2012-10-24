9e106e6e10f0e7ff3e21cecf52e437fb70070162
Roberto De Ioris <info@unbit.it>
2012-10-24 10:28:30 +0200

    improved efesto

--- efesto.py
+++ efesto.py
@@ -4,7 +4,10 @@ from dulwich.repo import Repo
 class Efesto:
 
     def render_html(self, page, start_response):
-        pass
+        start_response('200 OK', [('Content-Type','text/html')])
+        sha = self.git_index[page][8]
+        blob = self.repo.get_blob(sha)
+        return [self.get_header(page), blob.as_raw_string(), self.get_footer(page)]
 
     def render_rst(self, page, start_response):
         start_response('200 OK', [('Content-Type','text/html')])
@@ -15,9 +18,9 @@ class Efesto:
     def __init__(self, path='.'):
         self.repo = Repo(path)
         self.allowed_ext = {'html':self.render_html, 'rst':self.render_rst}
-        self.git_index = self.repo.open_index()
 
     def __call__(self, environ, start_response):
+        self.git_index = self.repo.open_index()
         requested_item = environ['PATH_INFO'][1:]
         if requested_item == '': requested_item = 'index'
         print requested_item
